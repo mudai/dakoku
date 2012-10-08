d4794e97049ad885eeaad96a228b5513eb1ebfdb
Takaaki Muta <takaaki.muta@gmail.com>
2012-10-08 15:14:27 +0900

    今の部分をコミットしておく

--- db/migrate/001_create_dakoku_datetimes.rb
+++ db/migrate/001_create_dakoku_datetimes.rb
@@ -2,9 +2,10 @@
 class CreateDakokuDatetimes < ActiveRecord::Migration
   def change
     create_table :dakoku_datetimes do |t|
-      t.date :dakoku_at # 打刻する日付（基本はstart_at, end_atと同じ日付となる）
+      t.integer :user_id
+      t.date :dakoku_at # 打刻する（した）日付（基本はstart_at, end_atと同じ日付となる）
       t.datetime :start_at # 出社時間
-      t.datetime :end_at # 退社時間　日をまたぐ可能性もあるので
+      t.datetime :end_at # 退社時間　日をまたぐ可能性もあるので(datetime)
       t.text :comment
       t.timestamps
     end
